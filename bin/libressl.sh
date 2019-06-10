f6023d016e2fe400eb9374d85bb2f61330adbc93
Laurent Coustet <laurent.coustet@clarisys.fr>
2019-06-10 17:26:49 +0200

    Try adding python 3.8 beta support

---
+++ bin/libressl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+ROOT=$(dirname $0)
+
+. $ROOT/common.sh
+
+LIBRESSL_VERSION=$($ROOT/checkupdate.sh -owner libressl-portable -repository portable -quiet)
+
+LIBRESSL_ROOT=libressl-${LIBRESSL_VERSION}
+LIBRESSL_DOWNLOAD_URL="https://ftp.openbsd.org/pub/OpenBSD/LibreSSL"
+
+
+fetch_source $LIBRESSL_ROOT.tar.gz $LIBRESSL_DOWNLOAD_URL
+tar -xzf $LIBRESSL_ROOT.tar.gz
+(
+    cd $LIBRESSL_ROOT
+    ./configure --enable-shared  CFLAGS=-fPIC --prefix=/usr/local/ssl
+    make -j $CPUS
+    make install
+)
